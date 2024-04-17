6af8ba75693cba7559af627a0516507957458844
mathewkoech <mathewkoech55@gmail.com>
2024-04-17 01:31:27 -0700

    DBStorage - States and Cities

--- console.py
+++ console.py
@@ -248,9 +248,8 @@ class HBNBCommand(cmd.Cmd):
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage.all().items():
+            for v in storage.all().values():
                 print_list.append(str(v))
-
         print(print_list)
 
     def help_all(self):
--- models/state.py
+++ models/state.py
@@ -2,21 +2,24 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+import os
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
 
 
+storage_type = os.getenv("STORAGE_TYPE")
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
     # For DBStorage
-    if models.storage_type == 'db':
+    if storage_type == 'db':
         cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     # For FileStorage
-    if models.storage_type == 'file':
+    if storage_type == 'file':
         @property
         def cities(self):
             """ Getter attribute for cities """
