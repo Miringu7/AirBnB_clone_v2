72eb45b9ce6c746a761d8ad2cbd099a3695b81c4
miringu7 <miringusimeon@gmail.com>
2024-04-17 16:01:02 +0300

    Revert "BaseModel, City, State update"
    This reverts commit 906e27352b8102197a7270b5e772099c854abc33.

--- models/state.py
+++ models/state.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    # For DBStorage
-    if models.storage_type == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    # For FileStorage
-    if models.storage_type == 'file':
-        @property
-        def cities(self):
-            """ Getter attribute for cities """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-                return city_list
+    name = ""
